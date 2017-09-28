2d79979e52777e60e4854da83a5aacf2f76d9228
Antonin Gaboriau <antonin.gaboriau@hotmail.fr>
2017-09-28 20:12:38 +0200

    Graphic: display of random points and a line in the canvas

--- perceptron1.py
+++ perceptron1.py
@@ -1,8 +1,15 @@
 from tkinter import *
+import random
 
-fenetre = Tk()
+fen = Tk()
 
-label = Label(fenetre, text="Hello World")
-label.pack()
+canvas = Canvas(fen, width=500, height=500, background='white')
+for i in range (50):
+    x = random.randint(0,500)
+    y = random.randint(0,500)
+    point = canvas.create_oval(x, y, x+5, y+5, fill='black')
+line = canvas.create_line(-100,150,600,400)
+    
+canvas.pack()
 
-fenetre.mainloop()
+fen.mainloop()
